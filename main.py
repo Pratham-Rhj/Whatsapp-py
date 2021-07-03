5141b42b283402d1917de155f469c7fea0cb1624
Pratham Raheja <unfazedguy2005@gmail.com>
2021-07-03 18:44:22 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+#type pip install pywhatkit in your terminal
+#importing pywhatkit
+import pywhatkit
+#sending message                                       time
+pywhatkit.sendwhatmsg('#enternumber', 'entermessage', 18, 52)
